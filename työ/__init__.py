fe219da99fb58ecc4b3fb755487ccc48a96c8b8b
Sagsunds <107113748+Sagsunds@users.noreply.github.com>
2024-06-16 00:07:42 +0300

    Update __init__.py

--- työ/__init__.py
+++ työ/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.config['SECRET_KEY'] = 'thisusfirstflaskapp'
 db = SQLAlchemy(app)
 
