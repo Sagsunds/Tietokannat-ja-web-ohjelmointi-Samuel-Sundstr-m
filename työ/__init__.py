6f1b0f5c1ec7f6812fe47471395e4cc77c84d4d1
Sagsunds <107113748+Sagsunds@users.noreply.github.com>
2024-06-15 22:29:17 +0300

    Add files via upload

--- työ/__init__.py
+++ työ/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sspostgres:kakkakakka@localhost:5432/ssflaskapp'
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = 'thisusfirstflaskapp'
 db = SQLAlchemy(app)
 
