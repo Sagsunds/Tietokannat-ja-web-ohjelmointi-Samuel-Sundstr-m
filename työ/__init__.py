553230f0e858cd49baf01a77f1d52df4f11bd5f1
Sagsunds <107113748+Sagsunds@users.noreply.github.com>
2024-06-29 19:52:52 +0300

    Add files via upload

--- työ/__init__.py
+++ työ/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from os import getenv
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://sspostgres:kakkakakka@localhost:5432/ssflaskapp'
 app.config['SECRET_KEY'] = 'thisusfirstflaskapp'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 db = SQLAlchemy(app)
+bcrypt=Bcrypt(app)
 
 from työ import routes
