9e63b3a13d7682309ad85f00c4be7f94ad248a19
Sagsunds <107113748+Sagsunds@users.noreply.github.com>
2024-06-30 19:11:12 +0300

    Update __init__.py

--- työ/__init__.py
+++ työ/__init__.py
@@ -4,8 +4,7 @@ from flask_bcrypt import Bcrypt
 from os import getenv
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://sspostgres:kakkakakka@localhost:5432/ssflaskapp'
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.config['SECRET_KEY'] = 'thisusfirstflaskapp'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 db = SQLAlchemy(app)
